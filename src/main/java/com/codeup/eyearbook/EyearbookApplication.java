1ae0611a9c7e22770bc1e44bcfc5b3b83c0f2834
Leroy Grant <lgrant146@gmail.com>
2020-08-13 16:34:23 -0500

    gitignore

---
+++ src/main/java/com/codeup/eyearbook/EyearbookApplication.java
@@ -0,0 +1,13 @@
+package com.codeup.eyearbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EyearbookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EyearbookApplication.class, args);
+    }
+
+}
