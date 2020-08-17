fd004a98cc95ca900550703425b6e55ce3025ca6
Leroy Grant <lgrant146@gmail.com>
2020-08-17 01:50:50 -0500

    security, jquery, bootstrap functionality and dependancies work

---
+++ src/main/java/com/codeup/eyearbook/controllers/HomeController.java
@@ -0,0 +1,15 @@
+package com.codeup.eyearbook.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/home")
+    public String test(){
+        return "index";
+    }
+
+}
