9062c7263cef491553f9612c482be7cae62002c3
Leroy Grant <lgrant146@gmail.com>
2020-08-18 14:40:09 -0500

    add auto generation id on student_records and seeder file

--- database/database.sql
+++ database/database.sql
@@ -6,3 +6,4 @@ USE eyearbook_db;
 create user 'eyearbook_user'@'localhost' IDENTIFIED BY 'codeup';
 GRANT ALL ON eyearbook_db.* TO 'eyearbook_user'@'localhost';
 
+
---
+++ database/seeder.sql
@@ -0,0 +1,11 @@
+# use eyearbook_db;
+use eyearbook_db;
+
+
+
+insert into signatures ( yearbook_comment, signer_id) values
+('Body 1 asdf asdf asd fasdf as dfa sdfa sdf', 1);
+
+
+insert into student_records (first_name, image, last_name, student_id) values
+('Leroy', 'path', 'grant', '0010100');
