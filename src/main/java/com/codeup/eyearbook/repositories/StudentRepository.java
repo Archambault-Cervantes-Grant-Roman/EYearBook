11d828cef5bf0188c10fda799e00237685cc04e4
Leroy Grant <lgrant146@gmail.com>
2020-08-20 11:31:03 -0500

    studentController and studentRepository

--- src/main/java/com/codeup/eyearbook/repositories/StudentRepository.java
+++ src/main/java/com/codeup/eyearbook/repositories/StudentRepository.java
@@ -5,13 +5,18 @@ import com.codeup.eyearbook.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
+
 
 public interface StudentRepository extends JpaRepository<Student, Long>  {
 
 
 
+    List<Student> findAllByOrderByIdDesc();
 
-    @Query (value = "select student_id, first_name, last_name FROM student_records where student_id = ?1", nativeQuery = true)
-   Student findStudentByStudentId(Student student_id);
+
+
+//    @Query (value = "select student_id, first_name, last_name FROM student_records where student_id = ?1", nativeQuery = true)
+//   Student findStudentByStudentId(Student student_id);
 
 }
