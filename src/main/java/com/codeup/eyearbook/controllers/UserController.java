8c4ddd557e25292bd187e352b0bf6932be8ab56c
AugustineCervantes <Augustine.CervantesIII@gmail.com>
2020-08-20 15:14:43 -0500

    adding some changes

--- src/main/java/com/codeup/eyearbook/controllers/UserController.java
+++ src/main/java/com/codeup/eyearbook/controllers/UserController.java
@@ -85,6 +85,15 @@ public class UserController {
         return "users/edit-profile";
     }
 
+    @PostMapping("edit-profile")
+    public String update(@PathVariable long id, @ModelAttribute User user) {
+        User loggedInUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+//        User user = usersDao.getOne(1L);
+        users.save(loggedInUser);
+        users.save(user);
+        return "redirect:/parent-profile";
+    }
+
     @GetMapping("/signature-page")
     public String signatureForm(Model model) {
         model.addAttribute("signatures", new Signatures());
@@ -115,16 +124,16 @@ public String childRegister(@PathVariable String parent_id, Model model) {
 //TODO:  need a post mapping to register the child - creating a new user
     //TODO:  post mapping redirect to parents-profile page
 
-    @PostMapping("/child-registration")
-    public String locateByStudentId(@ModelAttribute Student studentId){
-        student.
-        student.getStudent_id();
-    }
-
-    @PostMapping("child-registration")
-    public String saveStudent(@ModelAttribute User studentUser){
-
-    }
+//    @PostMapping("/child-registration")
+//    public String locateByStudentId(@ModelAttribute Student studentId){
+//        student.
+//        student.getStudent_id();
+//    }
+//
+//    @PostMapping("child-registration")
+//    public String saveStudent(@ModelAttribute User studentUser){
+//
+//    }
 
 
 //    User studentUser = new
