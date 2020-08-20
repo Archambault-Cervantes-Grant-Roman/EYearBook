73f660721344abba11fdde5c66dc03f806fa40a9
Leroy Grant <lgrant146@gmail.com>
2020-08-20 09:48:03 -0500

    search view

--- src/main/java/com/codeup/eyearbook/repositories/StudentRepository.java
+++ src/main/java/com/codeup/eyearbook/repositories/StudentRepository.java
@@ -1,7 +1,8 @@
 package com.codeup.eyearbook.repositories;
 
+import com.codeup.eyearbook.models.Student;
 import com.codeup.eyearbook.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface StudentRepository extends JpaRepository<User, Long>  {
+public interface StudentRepository extends JpaRepository<Student, Long>  {
 }
