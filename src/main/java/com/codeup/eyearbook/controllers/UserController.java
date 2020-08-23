7d5f5ae194018ea12c553cd442967dbff24c0a6e
Armando Roman <armando.roman.jr20@gmail.com>
2020-08-23 14:19:25 -0500

    had to fix coments issues

--- src/main/java/com/codeup/eyearbook/controllers/UserController.java
+++ src/main/java/com/codeup/eyearbook/controllers/UserController.java
@@ -47,16 +47,16 @@ public class UserController {
     }
 
     //TODO:this page needs to be dynamic between basic child and premium child
-    @GetMapping("/signature-page/{id}")
-    public String signaturePage(@PathVariable("id") long id, Model model){
-        // Armando: I have to have the following 3 lines
-        // to go to a users personal page and show their
-        // personal banner image
-        User user = users.getOne(id);
-        model.addAttribute("signatures", new Signatures());
-        model.addAttribute("user", user);
-        return "users/signature-page";
-    }
+//    @GetMapping("/signature-page/{id}")
+//    public String signaturePage(@PathVariable("id") long id, Model model){
+//        // Armando: I have to have the following 3 lines
+//        // to go to a users personal page and show their
+//        // personal banner image
+//        User user = users.getOne(id);
+//        model.addAttribute("signatures", new Signatures());
+//        model.addAttribute("user", user);
+//        return "users/signature-page";
+//    }
 
 
     @GetMapping("/parent-profile")
@@ -97,10 +97,16 @@ public class UserController {
 
     @GetMapping("/signature-page")
     public String signatureForm(Model model) {
+
         model.addAttribute("signatures", new Signatures());
         //Armando: inserted this attribute to be able to find and display comments
         model.addAttribute("comment", comment.findAll());
         //Armando: inserted this attribute to be able to find and display images
+        // Armando : not too sure if this belongs in the
+        // generic signature-page area
+        User loggedIn = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        User user = users.getOne(loggedIn.getId());
+        model.addAttribute("user", user);
         return "users/signature-page";
     }
 
@@ -109,7 +115,8 @@ public class UserController {
         User loggedInUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         signatures.setSigner(loggedInUser);
         System.out.println(loggedInUser.getUsername());
-
+        // new line to test if comments appear?
+        comment.save(signatures);
         System.out.println(signatures.getYearbook_comment());
         return "redirect:/signature-page";
     }
