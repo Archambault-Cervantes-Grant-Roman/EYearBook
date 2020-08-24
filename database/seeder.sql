761d71eaf26041d64947401856bcfcba5bf297b3
Bobbie Archambault <bmarchambault@gmail.com>
2020-08-24 16:40:22 -0500

    modified seeder for use on server

--- database/seeder.sql
+++ database/seeder.sql
@@ -1,5 +1,7 @@
+USE eyearbook_db;
+
 insert into student_records (first_name, image, last_name,  student_id) values
 ('Leroy', 'https://cdn.filestackcontent.com/BT3RYnEMTap6rZAJb9k6', 'Grant', '0010100'),
 ('Bobbie', 'https://cdn.filestackcontent.com/BT3RYnEMTap6rZAJb9k6', 'Archambault', '318808'),
-('Augustine', 'https://cdn.filestackcontent.com/BT3RYnEMTap6rZAJb9k6', 'Cervantes', '5743829'),
-('Armando', 'https://cdn.filestackcontent.com/BT3RYnEMTap6rZAJb9k6', 'Roman', '4234985');
+('Augustine', 'https://cdn.filestackcontent.com/BT3RYnEMTap6rZAJb9k6', 'Cervantes', '20200'),
+('Armando', 'https://cdn.filestackcontent.com/BT3RYnEMTap6rZAJb9k6', 'Roman', '30300');
