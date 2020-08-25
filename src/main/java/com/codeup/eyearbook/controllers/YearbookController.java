f40903bb8daaaebec10239a6ec5b6346fa4d1369
Leroy Grant <lgrant146@gmail.com>
2020-08-25 00:35:47 -0500

    yearbook pages added

--- src/main/java/com/codeup/eyearbook/controllers/YearbookController.java
+++ src/main/java/com/codeup/eyearbook/controllers/YearbookController.java
@@ -1,8 +1,10 @@
 package com.codeup.eyearbook.controllers;
 
+import com.codeup.eyearbook.models.User;
 import com.codeup.eyearbook.models.Yearbook;
 import com.codeup.eyearbook.repositories.UserRepository;
 import com.codeup.eyearbook.repositories.YearbookRepository;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -18,6 +20,17 @@ public class YearbookController {
 
     @RequestMapping("/yearbook")
     public String home(){
-        return "users/yearbook";
+        User loggedInUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        User user = new User();
+
+        if( !loggedInUser.isOwns_yearbook()){
+            return "/home";
+        }
+
+        if( loggedInUser.isOwns_yearbook()){
+
+        return "users/yearbook";}
+
+        return("/home");
     }
 }
