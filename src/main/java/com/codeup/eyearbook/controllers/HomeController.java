bc6df63c0ab2d71930d6634dd9e7f3af43069dee
Bobbie Archambault <bmarchambault@gmail.com>
2020-08-24 23:35:44 -0500

    tried to create user model for navbar dynamics

--- src/main/java/com/codeup/eyearbook/controllers/HomeController.java
+++ src/main/java/com/codeup/eyearbook/controllers/HomeController.java
@@ -1,14 +1,28 @@
 package com.codeup.eyearbook.controllers;
 
+import com.codeup.eyearbook.models.User;
+import com.codeup.eyearbook.repositories.UserRepository;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
+    private UserRepository users;
+
+    public HomeController(UserRepository users){
+        this.users = users;
+    }
+
+
     @RequestMapping("/")
-    public String home(){
+    public String home(Model model) {
+        User loggedInUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        User user = users.getOne(loggedInUser.getId());
+   model.addAttribute(user);
         return "home";
     }
 
