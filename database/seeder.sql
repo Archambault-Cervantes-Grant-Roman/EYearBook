508832f4a4ba3d6bad3a78ab24c6d404849f76f0
Armando Roman <armando.roman.jr20@gmail.com>
2020-08-26 18:58:49 -0500

    change our pic on seeder

--- database/seeder.sql
+++ database/seeder.sql
@@ -5,8 +5,8 @@
 USE eyearbook_db;
 TRUNCATE student_records;
 insert into student_records (first_name, image, last_name,  student_id) values
-('Leroy', 'https://cdn.filestackcontent.com/BT3RYnEMTap6rZAJb9k6', 'Grant', '0010100'),
-('Bobbie', 'https://cdn.filestackcontent.com/BT3RYnEMTap6rZAJb9k6', 'Archambault', '318808'),
-('Augustine', 'https://cdn.filestackcontent.com/BT3RYnEMTap6rZAJb9k6', 'Cervantes', '20200'),
-('Armando', 'https://cdn.filestackcontent.com/BT3RYnEMTap6rZAJb9k6', 'Roman', '30300');
+('Leroy', 'https://cdn.filestackcontent.com/ybzvHNRA6wXUtxB6V1EQ', 'Grant', '10100'),
+('Bobbie', 'https://cdn.filestackcontent.com/0kdK6M8DQDerTXdf5GHY', 'Archambault', '318808'),
+('Augustine', 'https://cdn.filestackcontent.com/GCEQLHtRvyVvzkEho4Ah', 'Cervantes', '20200'),
+('Armando', 'https://cdn.filestackcontent.com/8roWCAFPTmaH31EtieMY', 'Roman', '30300');
 
