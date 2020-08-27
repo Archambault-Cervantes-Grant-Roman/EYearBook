53e98c7dfb1cdeb21658752615780df0064173d4
Armando Roman <armando.roman.jr20@gmail.com>
2020-08-26 19:34:32 -0500

    updated student_records seeder

--- database/seeder.sql
+++ database/seeder.sql
@@ -8,5 +8,9 @@ insert into student_records (first_name, image, last_name,  student_id) values
 ('Leroy', 'https://cdn.filestackcontent.com/ybzvHNRA6wXUtxB6V1EQ', 'Grant', '10100'),
 ('Bobbie', 'https://cdn.filestackcontent.com/0kdK6M8DQDerTXdf5GHY', 'Archambault', '318808'),
 ('Augustine', 'https://cdn.filestackcontent.com/GCEQLHtRvyVvzkEho4Ah', 'Cervantes', '20200'),
-('Armando', 'https://cdn.filestackcontent.com/8roWCAFPTmaH31EtieMY', 'Roman', '30300');
+('Armando', 'https://cdn.filestackcontent.com/8roWCAFPTmaH31EtieMY', 'Roman', '30300'),
+('Ramon', 'https://cdn.filestackcontent.com/yJhXkP8S3A77KtrVVlAc', 'Villareal', '40400'),
+('Laura', 'https://cdn.filestackcontent.com/bIyJvBpkQgS1A1wuIcOq', 'Davis', '50500'),
+('Shelby', 'https://cdn.filestackcontent.com/t73F4KcuTgiVK0bdz3c3', 'Polasek', '60600'),
+('Knoah', 'https://cdn.filestackcontent.com/G5CLsdkQQGS3oPqznfEg', 'Cotto', '70700');
 
