2e9e2e1c4ab34540e11442cd1501841207a4e32a
AugustineCervantes <Augustine.CervantesIII@gmail.com>
2020-08-27 14:03:09 -0500

    updated seeder, home css

--- database/seeder.sql
+++ database/seeder.sql
@@ -5,8 +5,8 @@
 USE eyearbook_db;
 
 TRUNCATE student_records;
-insert into student_records (first_name, image, last_name,  student_id) values
 
+insert into student_records (first_name, image, last_name,  student_id) values
 ('Leroy', 'https://cdn.filestackcontent.com/ybzvHNRA6wXUtxB6V1EQ', 'Grant', '10100'),
 ('Bobbie', 'https://cdn.filestackcontent.com/0kdK6M8DQDerTXdf5GHY', 'Archambault', '318808'),
 ('Augustine', 'https://cdn.filestackcontent.com/GCEQLHtRvyVvzkEho4Ah', 'Cervantes', '20200'),
@@ -14,7 +14,18 @@ insert into student_records (first_name, image, last_name,  student_id) values
 ('Ramon', 'https://cdn.filestackcontent.com/yJhXkP8S3A77KtrVVlAc', 'Villareal', '40400'),
 ('Laura', 'https://cdn.filestackcontent.com/bIyJvBpkQgS1A1wuIcOq', 'Davis', '50500'),
 ('Shelby', 'https://cdn.filestackcontent.com/t73F4KcuTgiVK0bdz3c3', 'Polasek', '60600'),
-('Knoah', 'https://cdn.filestackcontent.com/G5CLsdkQQGS3oPqznfEg', 'Cotto', '70700');
-
-
-
+('Knoah', 'https://cdn.filestackcontent.com/G5CLsdkQQGS3oPqznfEg', 'Cotto', '70700'),
+('Aaron', 'https://cdn.filestackcontent.com/ybzvHNRA6wXUtxB6V1EQ', 'Singleterry', '80800'),
+('Andrew', 'https://cdn.filestackcontent.com/0kdK6M8DQDerTXdf5GHY', 'Bought', '90900'),
+('Dylan', 'https://cdn.filestackcontent.com/GCEQLHtRvyVvzkEho4Ah', 'Leifeste', '11100'),
+('Fernando', 'https://cdn.filestackcontent.com/8roWCAFPTmaH31EtieMY', 'Hernandez', '21200'),
+('George', 'https://cdn.filestackcontent.com/yJhXkP8S3A77KtrVVlAc', 'Williams', '31300'),
+('Gregory', 'https://cdn.filestackcontent.com/bIyJvBpkQgS1A1wuIcOq', 'Jett', '41400'),
+('Jahziel', 'https://cdn.filestackcontent.com/t73F4KcuTgiVK0bdz3c3', 'Baez', '51500'),
+('Jay', 'https://cdn.filestackcontent.com/G5CLsdkQQGS3oPqznfEg', 'Arredondo', '61600'),
+('Julia', 'https://cdn.filestackcontent.com/G5CLsdkQQGS3oPqznfEg', 'Contreras', '71700'),
+('Chris', 'https://cdn.filestackcontent.com/G5CLsdkQQGS3oPqznfEg', 'Eckert', '81800'),
+('Justin', 'https://cdn.filestackcontent.com/G5CLsdkQQGS3oPqznfEg', 'McCaleb', '91900'),
+('Larry', 'https://cdn.filestackcontent.com/G5CLsdkQQGS3oPqznfEg', 'Castillo', '12100'),
+('Michael', 'https://cdn.filestackcontent.com/G5CLsdkQQGS3oPqznfEg', 'Garrison', '22200'),
+('Trevor', 'https://cdn.filestackcontent.com/G5CLsdkQQGS3oPqznfEg', 'Mullis', '32300');
